c4e40dfc0f7879871cfe2460afbc9f39f4d001a9
s1260054@u-aizu.ac.jp <s1260054@u-aizu.ac.jp>
2020-06-16 18:30:40 +0900

    show list

---
+++ peek-lovefortune/app/Http/Controllers/FortuneController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Fortune;
+use App\Http\Requests\StoreFortuneRequest;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+use Throwable;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Admin;
+
+class FortuneController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return Factory|View
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Factory|View
+     */
+    public function create()
+    {
+        return view('createFortune');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     *
+     * @return Factory|View
+     * @throws Throwable
+     */
+    public function store(Request $request)
+    {
+        $fortune = new Fortune;
+        $fortune->my_name = $request->myName;
+        $fortune->my_crush_name = $request->myCrush;
+        $fortune->peek_user_id = $request->id;
+        $fortune->save();
+
+        $toMail = DB::table('users')->where('id', $fortune->peek_user_id)->value('email');
+
+        Mail::to($toMail)->send( new \App\Mail\Fortune($fortune->my_name, $fortune->my_crush_name) );
+
+        return redirect('/fortune/{id}/peeked');
+    }
+
+    /**
+     * Show the that it has been sent.
+     *
+     * @return Factory|View
+     */
+    public function peeked()
+    {
+        return view('peeked');
+    }
+
+}
--- peek-lovefortune/resources/views/home.blade.php
+++ peek-lovefortune/resources/views/home.blade.php
@@ -3,18 +3,48 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
+        <div class="w-100">
+            <div class="card m-3">
+                <div class="card-header">URL</div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    <div><a href="http://127.0.0.1/fortune/{{Auth::user()->id}}">http://127.0.0.1/fortune/{{Auth::user()->id}}</a></div>
+                </div>
+            </div>
+            <div class="card m-3">
+                <div class="card-header">過去の履歴</div>
+                <div class="card-body">
+                    ここに削除ボタンあると良いよね<br>
+                    表示件数多そうだからそこをページネーションに<br>
+                    登録情報の更新したいな
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th scope="col">占った方の名前</th>
+                                <th scope="col">占った方の好きな人の名前</th>
+                                <th scope="col">日付</th>
+                                <th scope="col">編集</th>
+                                <th scope="col">削除</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($fortunes as $fortune)
+                            <tr>
+                                <th scope="row">{{ $fortune_id++ }}</th>
+                                <td>{{ $fortune->my_name }}</td>
+                                <td>{{ $fortune->my_crush_name }}</td>
+                                <td>{{ $fortune->created_at }}</td>
+                                <td><input type="button" class="btn btn-info" value="編集"></td>
+                                <td><input type="button" class="btn btn-secondary" value="削除"></td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
